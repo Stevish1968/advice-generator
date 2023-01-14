b93a38e17cbb4a41ff5ae5dfe03c375730c02a96
Stephen Little <47438829+Stevish1968@users.noreply.github.com>
2023-01-14 12:26:47 -0400

    Added proper Javascript functionality to button

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
-// const adviceID = document.getElementById('advice-id');
-// const advice = document.getElementById('advice');
+const adviceID = document.getElementById("advice-id");
+const advice = document.getElementById("advice");
 
 const cardBody = document.getElementById("card");
+const dice = document.getElementById("dice");
 
-fetch("https://api.adviceslip.com/advice")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let adviceJSON = data;
-    const id = adviceJSON.slip.id;
-    const advice = adviceJSON.slip.advice;
+// function getAdvice() {
+//   fetch("https://api.adviceslip.com/advice")
+//   .then((response) => {
+//     return response.json();
+//   })
+// }
+//  console.log(getAdvice());
 
-    const card = `<div class="card-body">
+//     let adviceJSON = getAdvice();
+//     const id = adviceJSON.slip.id;
+//     const advice = adviceJSON.slip.advice;
+
+//     const card = `<div class="card-body">
+//  <h5>Advice #<span id="advice-id">${id}</span></h5>
+//  <p class="advice" id="advice"><q>${advice}</q></p>
+//  <div class="divider"><img src="images/pattern-divider-desktop.svg" alt="divider Image" /></div>
+// </div>`;
+
+//     cardBody.innerHTML = card;
+
+dice.addEventListener('click', () =>{loadJSON("https://api.adviceslip.com/advice")});
+loadJSON("https://api.adviceslip.com/advice");
+async function loadJSON(fname) {
+  var response = await fetch(fname);
+  var adviceJSON = await response.json();
+
+  const id = adviceJSON.slip.id;
+  const advice = adviceJSON.slip.advice;
+  const card = `<div class="card-body">
  <h5>Advice #<span id="advice-id">${id}</span></h5>
  <p class="advice" id="advice"><q>${advice}</q></p>
  <div class="divider"><img src="images/pattern-divider-desktop.svg" alt="divider Image" /></div>
 </div>`;
 
-    cardBody.innerHTML = card;
-  });
+  cardBody.innerHTML = card;
+}
